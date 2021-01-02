12e66c3b97c4c551ac91bc70b810166d30795f30
ZauryX <ZauryX@yandex.ru>
2021-01-02 23:19:45 +0300

    Игровой цикл

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+size = width, height = 800, 400
+screen = pygame.display.set_mode(size)
+pygame.init()
+pygame.display.set_caption('Жёлтый круг')
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
+pygame.quit()
